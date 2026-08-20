c28d307354b6568b69a09abe308ade11b3303211
Michael Crum <35321557+usedhondacivic@users.noreply.github.com>
2026-08-20 17:52:32 -0400

    Fix issue with controller speed limits (#16)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,8 +11,4 @@ source /opt/ros/noetic/setup.bash
 source /catkin_ws/devel/setup.bash
 
 exec roslaunch mote_demos simple_teleop_viz.launch \
-    robot_ip:="$ROBOT_IP" \
-    ${WHEEL_SEPARATION:+wheel_separation:="$WHEEL_SEPARATION"} \
-    ${WHEEL_RADIUS:+wheel_radius:="$WHEEL_RADIUS"} \
-    ${LASER_FRAME:+laser_frame:="$LASER_FRAME"} \
-    ${IMU_FRAME:+imu_frame:="$IMU_FRAME"}
+    robot_ip:="$ROBOT_IP"
